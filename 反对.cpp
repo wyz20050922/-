ebb217e557893f520b773fe55a59e151e34dc534
吴昀泽 <44499637+wyz20050922@users.noreply.github.com>
2018-10-29 21:51:04 +0800

    Update 反对.cpp

--- 反对.cpp
+++ 反对.cpp
@@ -1,11 +1,8 @@
-/**
- * @Date:   2018-04-24T17:22:36+08:00
- * @Last modified time: 2018-04-24T17:23:08+08:00
- */
 #include <iostream>
 #include <windows.h>
 using namespace std;
 int main()
 {
     system("shutdown /a");
+    return 0;
 }
