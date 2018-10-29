7a4c385a578106d1849868e7a7b248a894b3616f
吴昀泽 <44499637+wyz20050922@users.noreply.github.com>
2018-10-29 21:46:12 +0800

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,14 +1,8 @@
-/**
- * @Author: 吴昀泽
- * @Date:   2018-10-27T13:57:14+08:00
- * @Email:  15067000960@163.com
- * @Last modified by:   吴昀泽
- * @Last modified time: 2018-10-27T13:58:33+08:00
- */
 #include <iostream>
 using namespace std;
 int main()
 {
     cout << "hello word" << endl;
+    system("pause");
     return 0;
 }
